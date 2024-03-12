bfc368915847a25e777760cc7ff32ab08d8bd6c7
Vinncz <kevgun03@gmail.com>
2024-03-12 16:41:33 +0700

    Initial Commit

---
+++ LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by Vin on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
